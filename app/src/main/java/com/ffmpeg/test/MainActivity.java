e370e56420c4cf6957e20bfe4d23372a332a9543
lichuang <lichuang@hongyuanshidai.cn>
2017-03-29 15:15:09 +0800

    显示视频

--- app/src/main/java/com/ffmpeg/test/MainActivity.java
+++ app/src/main/java/com/ffmpeg/test/MainActivity.java
@@ -1,37 +1,72 @@
 package com.ffmpeg.test;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+
+    private SurfaceView surfaceView;
+    private SurfaceHolder mHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceview);
+        mHolder = surfaceView.getHolder();
+        mHolder.addCallback(this);
         final TextView infoText = (TextView) findViewById(R.id.text_libinfo);
         infoText.setMovementMethod(ScrollingMovementMethod.getInstance());
         Button button = (Button) this.findViewById(R.id.button_avcodec);
+        button.bringToFront();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                infoText.setText(avcodecinfo());
+                String input = new File(Environment.getExternalStorageDirectory(),"begin.mp4").getAbsolutePath();
+                Log.d("lichuang",input);
+                infoText.setText(codecinfo()+"");
             }
         });
     }
 
 
     //JNI
-    public native String avcodecinfo();
+    public native int avcodecinfo(String path,Object surface);
+    public native String codecinfo();
 
     static{
         System.loadLibrary("ffmpeg");
         System.loadLibrary("helloworld");
     }
 
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                avcodecinfo("/storage/emulated/0/sintel.mov",mHolder.getSurface());
+            }
+        }).start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
 }
--- app/src/main/jni/simplest_ffmpeg_helloworld.c
+++ app/src/main/jni/simplest_ffmpeg_helloworld.c
@@ -9,6 +9,12 @@
 #ifdef ANDROID
 #include <jni.h>
 #include <android/log.h>
+#include <libswscale/swscale.h>
+
+#include <android/native_window.h>
+#include <android/native_window_jni.h>
+#include <libavutil/imgutils.h>
+
 #define LOGE(format, ...)  __android_log_print(ANDROID_LOG_ERROR, "(>_<)", format, ##__VA_ARGS__)
 #else
 #define LOGE(format, ...)  printf("(>_<) " format "\n", ##__VA_ARGS__)
@@ -19,38 +25,164 @@
 struct URLProtocol;
 
 
-JNIEXPORT jstring Java_com_ffmpeg_test_MainActivity_avcodecinfo(JNIEnv *env, jobject obj)
+JNIEXPORT jint Java_com_ffmpeg_test_MainActivity_avcodecinfo(JNIEnv *env, jobject obj, jstring url, jobject surface)
 {
-	char info[40000] = { 0 };
-    sprintf(info,"lichuang");
-	av_register_all();
 
-	AVCodec *c_temp = av_codec_next(NULL);
+    // 视频url，这里有点小问题，外部传入的url，在这里转char *,有些问题，用了网上方法，jstring转char *也不靠谱，先mark下。
+    const char * file_name = ((*env)->GetStringUTFChars(env,url,NULL));
 
-	while(c_temp!=NULL){
-		if (c_temp->decode!=NULL){
-			sprintf(info, "%s[Dec]", info);
-		}
-		else{
-			sprintf(info, "%s[Enc]", info);
-		}
-		switch (c_temp->type){
-		case AVMEDIA_TYPE_VIDEO:
-			sprintf(info, "%s[Video]", info);
-			break;
-		case AVMEDIA_TYPE_AUDIO:
-			sprintf(info, "%s[Audio]", info);
-			break;
-		default:
-			sprintf(info, "%s[Other]", info);
-			break;
-		}
-		sprintf(info, "%s[%10s]\n", info, c_temp->name);
+    LOGE("start playvideo... url: %s",file_name);
+    av_register_all();
+    AVFormatContext * pFormatCtx = avformat_alloc_context();
+    // Open video file
+    if(avformat_open_input(&pFormatCtx, file_name, NULL, NULL)!=0) {
+        LOGE("Couldn't open file:%s\n", file_name);
+        return -1; // Couldn't open file
+    }
+    // Retrieve stream information
+    if(avformat_find_stream_info(pFormatCtx, NULL)<0) {
+        LOGE("Couldn't find stream information.");
+        return -1;
+    }
+    // Find the first video stream
+    int videoStream = -1, i;
+    for (i = 0; i < pFormatCtx->nb_streams; i++) {
+        if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO
+            && videoStream < 0) {
+            videoStream = i;
+        }
 
-		
-		c_temp=c_temp->next;
-	}
-	LOGE("%s", info);
 
-	return (*env)->NewStringUTF(env, info);
+    }
+    if(videoStream==-1) {
+        LOGE("Didn't find a video stream.");
+        return -1; // Didn't find a video stream
+    }
+    // Get a pointer to the codec context for the video stream
+    AVCodecContext  * pCodecCtx = pFormatCtx->streams[videoStream]->codec;
+    // Find the decoder for the video stream
+    AVCodec * pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
+    if(pCodec==NULL) {
+        LOGE("Codec not found. %d  %s" ,pCodecCtx->codec_id,pCodecCtx->codec_name);
+        return -1; // Codec not found
+    }
+    if(avcodec_open2(pCodecCtx, pCodec, NULL) < 0) {
+        LOGE("Could not open codec.");
+        return -1; // Could not open codec
+    }
+    // 获取native window
+    ANativeWindow* nativeWindow = ANativeWindow_fromSurface(env, surface);
+    // 获取视频宽高
+    int videoWidth = pCodecCtx->width;
+    int videoHeight = pCodecCtx->height;
+    // 设置native window的buffer大小,可自动拉伸
+    ANativeWindow_setBuffersGeometry(nativeWindow,  videoWidth, videoHeight, WINDOW_FORMAT_RGBA_8888);
+    ANativeWindow_Buffer windowBuffer;
+    if(avcodec_open2(pCodecCtx, pCodec, NULL)<0) {
+        LOGE("Could not open codec.");
+        return -1; // Could not open codec
+    }
+    // Allocate video frame
+    AVFrame * pFrame = av_frame_alloc();
+    // 用于渲染
+    AVFrame * pFrameRGBA = av_frame_alloc();
+    if(pFrameRGBA == NULL || pFrame == NULL) {
+        LOGE("Could not allocate video frame.");
+        return -1;
+    }
+    // Determine required buffer size and allocate buffer
+    // buffer中数据就是用于渲染的,且格式为RGBA
+    int numBytes=av_image_get_buffer_size(AV_PIX_FMT_RGBA, pCodecCtx->width, pCodecCtx->height, 1);
+    uint8_t * buffer=(uint8_t *)av_malloc(numBytes*sizeof(uint8_t));
+    av_image_fill_arrays(pFrameRGBA->data, pFrameRGBA->linesize, buffer, AV_PIX_FMT_RGBA,
+                         pCodecCtx->width, pCodecCtx->height, 1);
+    // 由于解码出来的帧格式不是RGBA的,在渲染之前需要进行格式转换
+    struct SwsContext *sws_ctx = sws_getContext(pCodecCtx->width,
+                                                pCodecCtx->height,
+                                                pCodecCtx->pix_fmt,
+                                                pCodecCtx->width,
+                                                pCodecCtx->height,
+                                                AV_PIX_FMT_RGBA,
+                                                SWS_BILINEAR,
+                                                NULL,
+                                                NULL,
+                                                NULL);
+    int frameFinished;
+    AVPacket packet;
+    int frameCount = 0;
+    while(av_read_frame(pFormatCtx, &packet)>=0) {
+        // Is this a packet from the video stream?
+        if(packet.stream_index==videoStream) {
+            // Decode video frame
+            avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, &packet);
+            // 并不是decode一次就可解码出一帧
+            if (frameFinished) {
+                LOGE("第%d帧, 图像格式：%d",frameCount,pFrame->format);
+                frameCount++;
+                // lock native window buffer
+                ANativeWindow_lock(nativeWindow, &windowBuffer, 0);
+                // 格式转换
+                sws_scale(sws_ctx, (uint8_t const * const *)pFrame->data,
+                          pFrame->linesize, 0, pCodecCtx->height,
+                          pFrameRGBA->data, pFrameRGBA->linesize);
+                // 获取stride
+                uint8_t * dst = windowBuffer.bits;
+                int dstStride = windowBuffer.stride * 4;
+                uint8_t * src = (uint8_t*) (pFrameRGBA->data[0]);
+                int srcStride = pFrameRGBA->linesize[0];
+                // 由于window的stride和帧的stride不同,因此需要逐行复制
+                int h;
+                for (h = 0; h < videoHeight; h++) {
+                    memcpy(dst + h * dstStride, src + h * srcStride, srcStride);
+                }
+                ANativeWindow_unlockAndPost(nativeWindow);
+            }
+        }
+        av_packet_unref(&packet);
+    }
+    av_free(buffer);
+    av_free(pFrameRGBA);
+    // Free the YUV frame
+    av_free(pFrame);
+    // Close the codecs
+    avcodec_close(pCodecCtx);
+    // Close the video file
+    avformat_close_input(&pFormatCtx);
+    ((*env)->ReleaseStringUTFChars(env,url,file_name));
+    return 0;
+}
+
+
+JNIEXPORT jstring Java_com_ffmpeg_test_MainActivity_codecinfo(JNIEnv *env, jobject obj) {
+    char info[40000] = { 0 };
+    av_register_all();
+
+    AVCodec *c_temp = av_codec_next(NULL);
+
+    while(c_temp!=NULL){
+        if (c_temp->decode!=NULL){
+            sprintf(info, "%s[Dec]", info);
+        }
+        else{
+            sprintf(info, "%s[Enc]", info);
+        }
+        switch (c_temp->type){
+            case AVMEDIA_TYPE_VIDEO:
+                sprintf(info, "%s[Video]", info);
+                break;
+            case AVMEDIA_TYPE_AUDIO:
+                sprintf(info, "%s[Audio]", info);
+                break;
+            default:
+                sprintf(info, "%s[Other]", info);
+                break;
+        }
+        sprintf(info, "%s[%10s] : %d\n", info, c_temp->name,c_temp->id);
+        int av = AV_PIX_FMT_YUYV422;
+        LOGE("hel: %d",av);
+        c_temp=c_temp->next;
+    }
+    LOGE("%s", info);
+
+    return (*env)->NewStringUTF(env, info);
 }
